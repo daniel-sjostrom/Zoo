84d42b6ed3e490760b0a926741a637489bc02a28
daniel-sjostrom <danielnsjostrom@gmail.com>
2023-10-31 21:57:53 +0100

    adjust commands

--- build-prod.sh
+++ build-prod.sh
@@ -3,7 +3,7 @@
 compose_files="-f docker-compose.base.yml -f docker-compose.prod.yml"
 
 # Build the services
-docker-compose $compose_files build
+docker compose $compose_files build
 
 # Start the services in detached mode
-docker-compose $compose_files up -d
+docker compose $compose_files up -d
