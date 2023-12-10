1ee739552b1c08251eff3965f7e4f0191e937964
daniel-sjostrom <danielnsjostrom@gmail.com>
2023-12-10 22:07:54 +0100

    remove unused code

--- fe/src/components/PostButton/PostButton.tsx
+++ fe/src/components/PostButton/PostButton.tsx
@@ -1,23 +1,13 @@
 "use client";
 
-import usePostStore from "@/stores/usePostStore";
 import styles from "./PostButton.module.css";
 
 const PostButton = () => {
-    const { data, post, isLoading, error } = usePostStore();
-
-    const postData = () => {
-        post("hello from the frontend5");
-    };
-
     return (
         <div>
-            <button className={styles.button} onClick={postData}>
+            <button className={styles.button}>
                 <p className={styles.buttonText}>Post</p>
             </button>
-            <p>{data?.message}</p>
-            <p>{isLoading}</p>
-            <p>{error}</p>
         </div>
     );
 };
