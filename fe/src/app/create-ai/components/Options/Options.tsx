3690aec7d17fb3cab3e41befd1ec7dcb8fbf23ba
daniel-sjostrom <danielnsjostrom@gmail.com>
2024-01-09 23:25:43 +0100

    render 2 ghost items when empty

--- fe/src/app/create-ai/components/Options/Options.tsx
+++ fe/src/app/create-ai/components/Options/Options.tsx
@@ -4,6 +4,7 @@ import commonStyles from "@/styles/common.module.css";
 import { AvailableModel } from "@/app/stores/useAIStore";
 
 import OptionItem from "./components/OptionItem";
+import GhostItem from "./components/GhostItem";
 
 interface Props {
     options: AvailableModel[] | undefined;
@@ -20,18 +21,28 @@ const Options: React.FC<Props> = (props) => {
         props.onSelect(selectedValue);
     };
 
+    if (props.options !== undefined) {
+        return (
+            <>
+                {props.options?.map((option, index) => (
+                    <div key={index}>
+                        <OptionItem
+                            onSelect={handleOptionClick}
+                            option={option}
+                            isSelected={selectedOption === option}
+                        />
+                        <div className={commonStyles.space2} />
+                    </div>
+                ))}
+            </>
+        );
+    }
+
     return (
         <>
-            {props.options?.map((option, index) => (
-                <div key={index}>
-                    <OptionItem
-                        onSelect={handleOptionClick}
-                        option={option}
-                        isSelected={selectedOption === option}
-                    />
-                    <div className={commonStyles.space2} />
-                </div>
-            ))}
+            <GhostItem />
+            <div className={commonStyles.space2} />
+            <GhostItem />
         </>
     );
 };
---
+++ fe/src/app/create-ai/components/Options/components/GhostItem/GhostItem.tsx
@@ -0,0 +1,7 @@
+import styles from "./GhostItem.module.css";
+
+export const GhostItem: React.FC = () => {
+    return <div className={styles.ghostItem} />;
+};
+
+export default GhostItem;
